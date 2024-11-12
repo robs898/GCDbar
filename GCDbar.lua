b88eb7c8ef4bf439f41d52102f5fff9a8421dd04
Robbie <rmackenzie8@gmail.com>
2024-11-12 21:43:33 +0000

    handle cancel casts

--- GCDbar.lua
+++ GCDbar.lua
@@ -1,6 +1,7 @@
 local GCD_DURATION = 1.5
 local startTime = 0
 local isOnGCD = false
+local isCasting = false
 
 local gcdBarFrame = CreateFrame("Frame", "GCDBarFrame", UIParent)
 gcdBarFrame:SetWidth(100)
@@ -22,19 +23,42 @@ bar:SetWidth(100)
 bar:SetHeight(10)
 bar:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
 
-gcdBarFrame:RegisterEvent("SPELLCAST_START")
-gcdBarFrame:SetScript("OnEvent", function()
+local function StartGCD()
     startTime = GetTime()
     isOnGCD = true
     gcdBarFrame:Show()
+end
+
+local function CompleteGCD()
+    isOnGCD = false
+    gcdBarFrame:Hide()
+end
+
+gcdBarFrame:RegisterEvent("SPELLCAST_START")
+gcdBarFrame:RegisterEvent("SPELLCAST_STOP")
+gcdBarFrame:RegisterEvent("SPELLCAST_FAILED")
+gcdBarFrame:RegisterEvent("SPELLCAST_INTERRUPTED")
+gcdBarFrame:SetScript("OnEvent", function()
+    if event == "SPELLCAST_START" then
+        isCasting = true
+        StartGCD()
+    elseif event == "SPELLCAST_STOP" then
+        if isCasting == false then
+            StartGCD()
+        else
+            CompleteGCD()
+            isCasting = false
+        end
+    else
+        CompleteGCD()
+    end
 end)
 
 gcdBarFrame:SetScript("OnUpdate", function()
     if isOnGCD then
         local elapsed = GetTime() - startTime
         if elapsed >= GCD_DURATION then
-            isOnGCD = false
-            gcdBarFrame:Hide()
+            CompleteGCD()
         else
             local progress = (GCD_DURATION - elapsed) / GCD_DURATION
             bar:SetWidth(100 * progress)
